d335da73951d80d478ec45e535c822b84edf7b46
prince <princekumar7320918928@gmail.com>
2025-07-28 01:52:50 +0530

    fill vpc

---
+++ terraform_05/main.tf
@@ -0,0 +1,158 @@
+#  1 : Create a VPC
+resource "aws_vpc" "myvpc"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "MyVPC"
+    }
+}
+
+#  2: Create a public subnet
+resource "aws_subnet" "PublicSubnet"{
+    vpc_id = aws_vpc.myvpc.id
+    availability_zone = "ap-south-1a"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "PublicSubnet"
+    }
+}
+
+#  3 : create a private subnet
+resource "aws_subnet" "PrivSubnet"{
+    vpc_id = aws_vpc.myvpc.id
+    availability_zone = "ap-south-1a"
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = false
+    tags = {
+        Name = "PrivSubnet"
+    }
+}
+
+
+#  4 : create IGW
+resource "aws_internet_gateway" "myIgw"{
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+        Name = "MyIgw"
+    }
+}
+
+#  5 : route Tables for public subnet
+resource "aws_route_table" "PublicRT"{
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myIgw.id
+    }
+    tags = {
+        Name = "PublicRT"
+    }
+}
+ 
+
+#  7 : route table association public subnet 
+resource "aws_route_table_association" "PublicRTAssociation"{
+    subnet_id = aws_subnet.PublicSubnet.id
+    route_table_id = aws_route_table.PublicRT.id
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "my-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-sg"
+  }
+}
+
+# create key pair
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = file("my_key.pub")
+}
+
+# create IAM role
+resource "aws_iam_role" "my_role" {
+  name = "my_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+# IAM Policy Attachment of EC2
+resource "aws_iam_role_policy_attachment" "ec2_policy" {
+  role       = aws_iam_role.my_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+# IAM Policy Attachment of S3
+resource "aws_iam_role_policy_attachment" "s3_policy" {
+  role       = aws_iam_role.my_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.my_role.name
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0f918f7e67a3323f0" # Ubuntu 22.04 LTS
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.PublicSubnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name               = aws_key_pair.my_key.key_name
+  user_data = file("install_nginx.sh")
+  tags = {
+    Name = "my-server"
+  }
+}
