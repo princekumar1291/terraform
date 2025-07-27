d1de6b7f0412f5fa1b787915bdda4d2e7baf12de
prince <princekumar7320918928@gmail.com>
2025-07-27 17:29:46 +0530

    .

--- terraform_02/outputs.tf
+++ terraform_02/outputs.tf
@@ -1,19 +1,3 @@
-# output "ec2_public_ip" {
-#   value = aws_instance.my_instance[*].public_ip
-# }
-
-# output "ec2_public_dns" {
-#   value = aws_instance.my_instance[*].public_dns
-# }
-
-# output "ec2_private_ip" {
-#   value = aws_instance.my_instance[*].private_ip
-# }
-
-# output "ec2_region" {
-#   value = aws_instance.my_instance[*].availability_zone
-# }
-
 output "ec2_public_ips" {
   value = {
     for key, instance in aws_instance.my_instance :
